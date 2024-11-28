bd1d3d78a2b43c4f5c14081bdeec1a0231fd61a7
liwenyao <liwenyao>
2024-11-28 18:33:48 +0800

    lwy

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
 import gradio as gr
 import traceback
+import re
+
+def clean_func(content, filter=[(" ", "")]):
+    """清除头尾指定字符"""
+    for i in filter:
+        while 1:
+            if content[0] == i[0]:
+                content = content.replace(i[0], i[1])
+            elif content[-1] == i[0]:
+                content = content.replace(i[0], i[1])
+            else:
+                break
+    return content
+
+
+def html_extracter(context):
+    re_pattern = "<p>(.*?)<"
+    re_exc = re.findall(re_pattern, context.replace("\n", ""))
+
+    res = []
+    for re_exc_i in re_exc:
+        res.append(clean_func(re_exc_i))
+    return "\n".join(res), "SUCCESS"
 
 
 def hello_world_fn(username: str) -> tuple[str, str]:
@@ -35,6 +58,18 @@ def main() -> None:
                 outputs=[pack_output, status_output],
             )
 
+        with gr.Tab("hello world 2"):
+            raw_input = gr.Textbox(lines=1, placeholder="输入你的名字(英文)", label="")
+            pack_output = gr.Textbox(label="输出")
+            status_output = gr.Textbox(label="状态信息")
+
+            btn = gr.Button("开始转换")
+            btn.click(
+                fn=html_extracter,
+                inputs=raw_input,
+                outputs=[pack_output, status_output],
+            )
+
     demo.queue(default_concurrency_limit=100).launch(
         inline=False,
         debug=False,
@@ -46,3 +81,5 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
+    # print(html_extracter(text))
